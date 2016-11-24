be8274354ee6e749148d81b8afc501afe3ca09e4
Daytona <Daytona@10.248.142.215>
2016-11-24 22:57:55 +1100

    modified lockcontroller

--- LockinController/src/LockinController.py
+++ LockinController/src/LockinController.py
@@ -10,6 +10,7 @@ import numpy as np
 import pandas as pd
 
 if __name__ == '__main__':
+    time_start = time.time()
     # 1. link to lock-in
     rm = visa.ResourceManager()
     inst = rm.open_resource("GPIB0::23::INSTR")
@@ -18,11 +19,11 @@ if __name__ == '__main__':
     fiber_type = "P1202ARSLS-14s"
     irradiation_power = "40mW"
     timeNow = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime(time.time()))
-    irradiation_time_expect = 80
+    irradiation_time_expect = 60*60*5
+    
     fileName = './' + timeNow + '_PhotoBleaching_' + fiber_type + '_' + irradiation_power + '.csv'
-    time_start = time.time()
     irradiation_time = 0
-    result = pd.DataFrame(columns = ['time','irradiation_time','intensity','frequency'])
+    result = pd.DataFrame(columns = ['time','irradiation_time (sec)','intensity (mV)','frequency (Hz)'])
     index = 0
     
     while irradiation_time < irradiation_time_expect:
@@ -30,11 +31,16 @@ if __name__ == '__main__':
         time_now = time.time()
         irradiation_time = time_now - time_start
         
-        #intensity = 1.0
-        #frequency = 133
-        
         intensity = inst.query('Q1')
         frequency = inst.query('f')
+        #intensity = "112.45E-6\r\n"
+        #frequency = "133\r\n"
+        
+        value = float(intensity.rsplit("E")[0])
+        power = float(intensity.rsplit("E")[1])
+        intensity = value / (np.power(10,-1*power))
+        frequency = float(frequency)
+        
         strnow = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         result.loc[index] = [strnow,irradiation_time,intensity,frequency]
         index += 1
